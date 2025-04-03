307f9ae5fce655e7ef20c250745cfc63d5c12b94
Derek Raven <derek.raven@nventive.com>
2025-04-03 12:03:09 -0400

    End of Section 11: Adding photo upload functionality

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -18,7 +19,9 @@ public static class ApplicationServiceExtensions
         serviceCollection.AddCors();
         serviceCollection.AddScoped<ITokenService, TokenService>();
         serviceCollection.AddScoped<IUserRepository, UserRepository>();
+        serviceCollection.AddScoped<IPhotoService, PhotoService>();
         serviceCollection.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        serviceCollection.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
         
         return serviceCollection;
     }
